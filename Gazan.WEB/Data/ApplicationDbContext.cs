9524d4384077a2cce508ae5a4552380e279fc1d3
Yevhenii Kovalenko <jeikkowalenko@gmail.com>
2020-12-05 03:14:33 +0200

    Add project files.

---
+++ Gazan.WEB/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Gazan.WEB.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gazan.WEB.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
